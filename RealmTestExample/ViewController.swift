b74119aa0f01314631d598dacfed0a735236182d
Corey Walo <cjwalo@gmail.com>
2017-02-20 10:51:15 -0600

    Initial Commit

---
+++ RealmTestExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmTestExample
+//
+//  Created by Corey Walo on 2/20/17.
+//  Copyright © 2017 Corey Walo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
